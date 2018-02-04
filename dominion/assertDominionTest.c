49030464936bfe47b35bac9c162889ffb4c1b220
jonah1217 <spencjon@oregonstate.edu>
2018-02-03 16:48:44 -0800

    bug fixes

--- dominion/assertDominionTest.c
+++ dominion/assertDominionTest.c
@@ -31,34 +31,46 @@ void printGameState(struct gameState *state)
 
 
 
-bool assertGameStateDom(bool, string, struct gameState *pre, struct gameState *post)
+bool assertGameStateDom(bool assertion, string MSG, struct gameState *pre, struct gameState *post)
   {
-    if(!assertion){
-    fprintf(stderr,"Errormessage: %s", MSG);
-    cout << "\nAssertion Failed:" << MSG;
-    cout << "\n \n*************PRE*******************"
-    printGameState(pre)
-    cout << "\n \n*************POST******************"
-    printGameState(post)
-    cout << "\n \n"
-    cout << "Exit? (Y/N)";
-    char choice;
-    cin << choice;
-    if(choice == Y || choice == y){
-      return 1;
+    if(!assertion)
+    {
+      fprintf(stderr,"Errormessage: %s", MSG);
+      cout << "\nAssertion Failed:" << MSG;
+      cout << "\n \n*************PRE*******************";
+      printGameState(pre);
+      cout << "\n \n*************POST******************";
+      printGameState(post);
+      cout << "\n \n";
+      cout << "Exit? (Y/N)";
+      char choice;
+      cin >> choice;
+      if(choice == 'Y' || choice == 'y'){
+        return 1;
+      }
+      else
+      {
+        return 0;
+      }
     }
     return 0;
 }
 
-bool assertStandardDom(bool assertion, string MSG){
+bool assertStandardDom(bool assertion, string MSG)
+{
   if(!assertion){
     fprintf(stderr,"Errormessage: %s", MSG);
     cout << "Assertion Failed:" << MSG;
     cout << "Exit? (Y/N)";
     char choice;
     cin >> choice;
-    if(choice == Y || choice == y){
+    if(choice == 'Y' || choice == 'y'){
       return 1;
     }
-    return 0;
+    else
+    {
+      return 0;
+    }
+  }
+  return 0;
 }
