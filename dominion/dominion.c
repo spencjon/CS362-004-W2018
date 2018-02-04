b329f83e45fd88f12a9ac7371863074ce1bcbef7
jonah1217 <spencjon@oregonstate.edu>
2018-02-03 16:27:02 -0800

    edited changes from gcc to g++ explicit change from void* to structgametstate */int*

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -14,13 +14,13 @@ int compare(const void* a, const void* b) {
 }
 
 struct gameState* newGame() {
-  struct gameState* g = malloc(sizeof(struct gameState));
+  struct gameState* g = (struct gameState*) malloc(sizeof(struct gameState));
   return g;
 }
 
 int* kingdomCards(int k1, int k2, int k3, int k4, int k5, int k6, int k7,
 		  int k8, int k9, int k10) {
-  int* k = malloc(10 * sizeof(int));
+  int* k = (int *)malloc(10 * sizeof(int));
   k[0] = k1;
   k[1] = k2;
   k[2] = k3;
