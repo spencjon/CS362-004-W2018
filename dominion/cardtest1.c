0a1de32a165897b69bbfdc2f2a3487420bc6bb06
jonah1217 <spencjon@oregonstate.edu>
2018-02-04 19:23:43 -0800

    asd

---
+++ dominion/cardtest1.c
@@ -0,0 +1,63 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include "assertDominionTest.h"
+
+int checkSmithyCard(int currentPlayer, struct gameState* gameReturned, struct gameState* gameExpected){
+    int expected, returned;
+
+    returned = playCard(0,1,1,1,gameReturned);
+
+    assertStandardDom(returned == -1, "Play Card Failed"){
+        return 1;
+    }
+
+    gameExpected->handCount[currentPlayer] += 3;
+    gameExpected->deckCount[currentPlayer] -= 3;
+
+    if (assertStandardDom(gameReturned->deckCount[currentPlayer] == gameExpected->deckCount[currentPlayer], "Deck Count Varies")){
+        return 1
+    }
+    if (assertStandardDom(gameReturned->handCount[currentPlayer] == gameExpected->handCount[currentPlayer], "Hand Count Varies"))
+
+    return 0;
+}
+
+
+int main () {
+
+  int p, i, returned, expected;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  struct gameState G, F;
+
+  printf ("Testing discardCard.\n");
+
+
+  printf ("SIMPLE FIXED TESTS: discardCard(...)\n");
+  for (p = 2; p < 5; p++)
+  {
+    for(i = 0; i < 4; i++)
+    {
+        initializeGame(2, k, 1, &G);
+        G->hand[i][0] = 13; //place smithy card into hand
+        G->whoseTurn = i; //it's that person's turn
+        F = G;
+        
+        if(checkSmithyCard(i, &G, &F))
+        {
+            printf("\n****ERROR: Tests Failed, requeted return****\n");
+            return 1;
+        } 
+    }
+    printf("\nTest for numPlayers = %i has passed\n", p);
+  }
+  printf("\nAll tests have passed\n");
+
+  return 1;
+}
