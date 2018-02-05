49e469e79a03fd6ac93df6a187ee29ced363e560
jonah1217 <spencjon@oregonstate.edu>
2018-02-04 18:32:31 -0800

    asd

--- dominion/unittest4.c
+++ dominion/unittest4.c
@@ -10,13 +10,13 @@ int checkIsGameOver(struct gameState* state) {
     
     returned = isGameOver(&modify);  
     
-    if(assertStandardDom((returned == 0), "Just Initialized")){
+    if(assertStandardDom((returned == 0), "Just Initialized\n")){
         return 1;
     }
 
     modify.supplyCount[province] = 0;
     returned = isGameOver(&modify);  
-    if(assertStandardDom((returned == 1), "Provinces == 0")){
+    if(assertStandardDom((returned == 1), "Provinces == 0\n")){
         return 1;
     }
 
@@ -25,11 +25,11 @@ int checkIsGameOver(struct gameState* state) {
     modify.supplyCount[adventurer] = 0;
     modify.supplyCount[great_hall] = 0;
     returned = isGameOver(&modify);  
-    if(assertStandardDom((returned == 1), "3 supply piles are empy")){
+    if(assertStandardDom((returned == 1), "3 supply piles are empy\n")){
         return 1;
     }
 
-    printf("Tests passed");
+    printf("All Tests passed\n");
     return 0;
 }
 
@@ -43,16 +43,16 @@ int main(){
 
   for(p = 2; p < 5; p++){
       r = initializeGame(p, k, 1, &G);
-      if(assertStandardDom((r == 0), "initializeGame failed")){
-        printf("Return Requested.. Returning... ");
+      if(assertStandardDom((r == 0), "initializeGame failed\n")){
+        printf("Return Requested.. Returning... \n");
         return 1;
       }
       if(checkIsGameOver(&G)){
-        printf("Return Requested.. Returning...");
+        printf("Return Requested.. Returning...\n");
         return 1;
     }
-    printf("Tests passed for %i players", p);
+    printf("Tests passed for %i players\n", p);
   }
-  printf("Tests passed for all number of players");
+  printf("Tests passed for all number of players\n");
   return 0;
 }
