7cb2f8f24ab9bbee0af717a94e522547fd302a14
jonah1217 <spencjon@oregonstate.edu>
2018-02-17 21:44:01 -0800

    l

--- projects/spencjon/dominion/rngs.h
+++ projects/spencjon/dominion/rngs.h
@@ -6,8 +6,6 @@
  * ----------------------------------------------------------------------- 
  */
 
-#if !defined( _RNGS_ )
-#define _RNGS_
 
 double Random(void);
 void   PlantSeeds(long x);
@@ -15,5 +13,3 @@ void   GetSeed(long *x);
 void   PutSeed(long x);
 void   SelectStream(int index);
 void   TestRandom(void);
-
-#endif
