b0d5887032eea175675a1961d0dd586377a870c3
jonah1217 <spencjon@oregonstate.edu>
2018-02-18 23:08:30 -0800

    asd

--- projects/spencjon/dominion/randomtestadventurer.c
+++ projects/spencjon/dominion/randomtestadventurer.c
@@ -67,8 +67,8 @@ int checkAdventurer(int p, struct gameState *post){
   //printf("Past %s \n", "posthand num treasuer");
 
   if(numTreasure > 1){
-    //printf("HandCount Post: %i, Pre %i \n",post->handCount[p],pre.handCount[p]);
-    //printf("TreasureDelta: Post %i, PRe %i \n", postHandTreasure, preHandTreasure);
+    printf("HandCount Post: %i, Pre %i \n",post->handCount[p],pre.handCount[p]);
+    printf("TreasureDelta: Post %i, PRe %i \n", postHandTreasure, preHandTreasure);
     assertStandardDom(((pre.handCount[p]) + 1) == (post->handCount[p]), "Hand count incorrect: enough treasure.");
     assertStandardDom(((postHandTreasure - 2) == preHandTreasure), "Incorrect Number of pre/post hand treasure delta");
   } else if(numTreasure == 1){
